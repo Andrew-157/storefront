5887fdd68c1acd05050b623c5cad7e761aa1a498
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 13:16:47 +0200

    examples of annotating

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
-from django.db.models import Value, F, ExpressionWrapper, DecimalField
-from store.models import Product
+from django.db.models import Value, F, Count, Max, Sum
+from store.models import Customer, Collection, Product
 
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(
-        F('unit_price') * 0.8, output_field=DecimalField())
-    queryset = Product.objects.annotate(
-        discounted_price=discounted_price
+    # Customers and their last order id
+    queryset = Customer.objects.annotate(last_order_id=Max('order__id'))
+    # Collections and count of their products
+    queryset = Collection.objects.annotate(count_products=Count('product'))
+    # Customers with more than 5 orders
+    queryset = Customer.objects.annotate(
+        count_orders=Count('order')).filter(count_orders__gt=5)
+    # Customers and total amount they spent
+    queryset = Customer.objects.annotate(
+        total_spent=Sum(F('order__orderitem__unit_price') *
+                        F('order__orderitem__quantity'))
     )
+    # Top 5 best-selling products and their total sales
+    queryset = Product.objects.annotate(
+        total_sales=Sum(
+            F('orderitem__unit_price') * F('orderitem__quantity')
+        )
+    ).order_by('-total_sales')[:5]
     return render(request, 'hello.html', {'name': 'Andrew', 'result': list(queryset)})
