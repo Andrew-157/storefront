ec9f975c4bb39f74428e2e442025ef677086e5a8
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 12:24:30 +0200

    Aggregate objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.db.models import Q, F
-from store.models import Product, OrderItem, Order
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from store.models import Product
 
 
 def say_hello(request):
-    queryset = Order.objects.select_related(
-        'customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-    return render(request, 'hello.html', {'name': 'Andrew', 'orders': list(queryset)})
+    result = Product.objects.aggregate(
+        count=Count('id'), min_price=Min('unit_price'))
+    result = Product.objects.filter(collection__id=6).aggregate(
+        count=Count('id'), min_price=Min('unit_price'))
+    return render(request, 'hello.html', {'name': 'Andrew', 'result': result})
