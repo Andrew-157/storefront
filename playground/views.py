f0870d13a50c8178afa02b7d881e2482593d4d52
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 10:53:36 +0200

    another method of using AND clause

--- playground/views.py
+++ playground/views.py
@@ -4,5 +4,6 @@ from store.models import Product
 
 def say_hello(request):
     # inventory < 10 AND unit price < 20
-    queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    queryset = Product.objects.filter(
+        inventory__lt=10).filter(unit_price__lt=20)
     return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset)})
