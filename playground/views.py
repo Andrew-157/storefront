8e0f81ba363006c205a7285ff7e193b472d599ce
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 14:05:28 +0200

    creating transaction with decorator

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.db import transaction
 from store.models import Order, OrderItem, Product, Customer, Collection
 
 
+@transaction.atomic()
 def say_hello(request):
-    collection = Collection(pk=11)
-    collection.delete()
-    Collection.objects.filter(id__gt=5).delete()
+    order = Order()
+    order.customer_id = 1
+    order.save()
+
+    item = OrderItem()
+    item.order = order
+    item.product_id = 1
+    item.quantity = 1
+    item.unit_price = 10
+    item.save()
     return render(request, 'hello.html', {'name': 'Andrew'})
