9768232fc90bb59c1daf76eb9a4ca45942d2d6ca
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 11:41:09 +0200

    deferring fields

--- playground/views.py
+++ playground/views.py
@@ -4,6 +4,6 @@ from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    ordered = OrderItem.objects.values_list('product_id').distinct()
-    queryset = Product.objects.filter(id__in=ordered).order_by('title')
+    queryset = Product.objects.only('id', 'title')
+    queryset = Product.objects.defer('description')
     return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset)})
