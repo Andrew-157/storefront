98d025023b6f9256e3df8b84fdfa508270997354
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 10:39:48 +0200

    checking nullable

--- playground/views.py
+++ playground/views.py
@@ -3,5 +3,5 @@ from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(last_update__year=2021)
+    queryset = Product.objects.filter(description__isnull=True)
     return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset)})
