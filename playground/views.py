1ecd20e3f8716504f5a752ff0eaa5199d8e9b475
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 10:51:52 +0200

    using and clause

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from store.models import Product, Customer, Collection, Order, OrderItem
+from store.models import Product
 
 
 def say_hello(request):
-    queryset = Customer.objects.filter(email__contains='.com')
-    queryset_1 = Collection.objects.filter(featured_product__isnull=True)
-    queryset_2 = Product.objects.filter(inventory__lt=10)
-    queryset_3 = Order.objects.filter(customer__id=1)
-    queryset_4 = OrderItem.objects.filter(product__collection__id=3)
-    return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset_4)})
+    # inventory < 10 AND unit price < 20
+    queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset)})
