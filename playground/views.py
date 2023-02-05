a7994c000b0776080d39187ab845ca0f03cfb3bf
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 10:47:58 +0200

    examples of using lookup types

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, Customer, Collection, Order, OrderItem
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(description__isnull=True)
-    return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset)})
+    queryset = Customer.objects.filter(email__contains='.com')
+    queryset_1 = Collection.objects.filter(featured_product__isnull=True)
+    queryset_2 = Product.objects.filter(inventory__lt=10)
+    queryset_3 = Order.objects.filter(customer__id=1)
+    queryset_4 = OrderItem.objects.filter(product__collection__id=3)
+    return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset_4)})
