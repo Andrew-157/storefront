9e496ba7cb413c90015e801c9be8d5a1e02403bf
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 12:41:44 +0200

    shorter way to call db functions

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.db.models import Value, F, Func
+from django.db.models.functions import Concat
 from store.models import Customer
 
 
 def say_hello(request):
     queryset = Customer.objects.annotate(
-        full_name=Func(F('first_name'), Value(
-            ' '), F('last_name'), function='CONCAT')
+        full_name=Concat('first_name', Value(' '), 'last_name')
     )
     return render(request, 'hello.html', {'name': 'Andrew', 'result': list(queryset)})
