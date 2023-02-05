211becb408ec5e17f4d2d030f34005b26de84c06
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 11:30:56 +0200

    example of using specific fields

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    queryset = Product.objects.values('id', 'title', 'collection__title')
+    ordered = OrderItem.objects.values_list('product_id').distinct()
+    queryset = Product.objects.filter(id__in=ordered).order_by('title')
     return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset)})
