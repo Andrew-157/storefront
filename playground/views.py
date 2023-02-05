6a3fd0ece0e5bc04f61a97a65c09fcb31f1855b2
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 11:02:04 +0200

    Referencing fileds Using F objects

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 
 def say_hello(request):
-    # inventory < 10 OR unit price < 20
-    queryset = Product.objects.filter(
-        Q(inventory__lt=10) | ~Q(unit_price__lt=20))
+    queryset = Product.objects.filter(inventory=F('unit_price'))
     return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset)})
