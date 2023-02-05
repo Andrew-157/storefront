cdda3dad8756e33769c3588feaaebbc1f169443c
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 12:36:39 +0200

    Calling Database Functions

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.db.models import Value, F
+from django.db.models import Value, F, Func
 from store.models import Customer
 
 
 def say_hello(request):
     queryset = Customer.objects.annotate(
-        new_id=F('id')).annotate(is_new=Value(True))
+        full_name=Func(F('first_name'), Value(
+            ' '), F('last_name'), function='CONCAT')
+    )
     return render(request, 'hello.html', {'name': 'Andrew', 'result': list(queryset)})
