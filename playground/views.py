2106fb442526382af8dddf2b723a1c3d26c18f95
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 12:09:13 +0200

    usage of selecting related objects
    select_related 1 - 1 (May be Foreign key or OnetoOne relationship)
    prefetch_related 1- n (ManyToMany relationship or something like this)

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 
 def say_hello(request):
-    queryset = Product.objects.prefetch_related(
-        'promotions').select_related('collection').all()
-    return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset)})
+    queryset = Order.objects.select_related(
+        'customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    return render(request, 'hello.html', {'name': 'Andrew', 'orders': list(queryset)})
