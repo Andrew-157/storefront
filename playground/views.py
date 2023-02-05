dedc09219a0cd8a5b74493f2a32c04462bc8f226
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 11:09:55 +0200

    some shrotcuts for sorting

--- playground/views.py
+++ playground/views.py
@@ -4,5 +4,8 @@ from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.order_by('unit_price', '-title').reverse()
+    # sorts result by unit_price and returns first object
+    queryset = Product.objects.earliest('unit_price')
+    # sorts result by unit_price and returns last object
+    queryset = Product.objects.latest('unit_price')
     return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset)})
