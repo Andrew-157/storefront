99ad3c68ca83c66b99d95ab537eac9509fb3df33
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 13:48:30 +0200

    Creating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.contrib.contenttypes.models import ContentType
-from store.models import Product
-from tags.models import TaggedItem
+from store.models import Order, OrderItem, Product, Customer, Collection
 
 
 def say_hello(request):
-    queryset = TaggedItem.objects.get_tags_for(Product, 1)
-    return render(request, 'hello.html', {'name': 'Andrew', 'tags': list(queryset)})
+    collection = Collection()
+    collection.title = 'Video Games'
+    collection.featured_product = Product(pk=1)
+    collection.save()
+    return render(request, 'hello.html', {'name': 'Andrew'})
