4e54ae82debd66b191bfbcfdde7761a868d18d0a
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 13:54:27 +0200

    update object with reading it first

--- playground/views.py
+++ playground/views.py
@@ -3,8 +3,7 @@ from store.models import Order, OrderItem, Product, Customer, Collection
 
 
 def say_hello(request):
-    collection = Collection()
-    collection.title = 'Video Games'
-    collection.featured_product = Product(pk=1)
+    collection = Collection.objects.get(pk=11)
+    collection.featured_product = None
     collection.save()
     return render(request, 'hello.html', {'name': 'Andrew'})
