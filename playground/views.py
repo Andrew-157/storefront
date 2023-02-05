13b5f96f40bf3662b02024c743d0ff1d794de129
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 10:28:10 +0200

    Use field lookups to filter data

--- playground/views.py
+++ playground/views.py
@@ -3,5 +3,5 @@ from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(pk=0).exists()
+    queryset = Product.objects.filter(unit_price__gt=20)
     return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset)})
