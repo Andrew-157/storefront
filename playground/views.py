8b51ffbc994ac92d8f8dff14ad5a69f94f956027
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 13:56:40 +0200

    updating without first reading

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,8 @@ from store.models import Order, OrderItem, Product, Customer, Collection
 
 
 def say_hello(request):
-    collection = Collection.objects.get(pk=11)
-    collection.featured_product = None
-    collection.save()
+    # collection = Collection.objects.get(pk=11)
+    # collection.featured_product = None
+    # collection.save()
+    Collection.objects.filter(pk=11).update(featured_product=None)
     return render(request, 'hello.html', {'name': 'Andrew'})
