7010cb423ac0b9577253f7655f595e21d9d11345
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 14:20:24 +0200

    use context manager and cursor to write raw SQL

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import connection
 from store.models import Order, OrderItem, Product, Customer, Collection
 
 
 def say_hello(request):
-    # this queryset is different from the previous
-    # it doesn't have a lot of methods like filter, annotate etc.
-    queryset = Product.objects.raw('SELECT * FROM store_product')
+    with connection.cursor() as cursor:
+        cursor.execute('')
+        cursor.callproc()  # this executes stored procedures
     return render(request, 'hello.html', {'name': 'Andrew', 'result': list(queryset)})
