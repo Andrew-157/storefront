d7e0d0767e631ffb87042bfbdfac92261a18f8ef
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 12:47:40 +0200

    grouping data

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func
-from django.db.models.functions import Concat
+from django.db.models import Value, F, Func, Count
 from store.models import Customer
 
 
 def say_hello(request):
     queryset = Customer.objects.annotate(
-        full_name=Concat('first_name', Value(' '), 'last_name')
+        orders_count=Count('order')
     )
     return render(request, 'hello.html', {'name': 'Andrew', 'result': list(queryset)})
