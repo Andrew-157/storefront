2e100c0b0244dcd79b39c8b943d9dafb83447b22
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-05 11:17:26 +0200

    limiting results

--- playground/views.py
+++ playground/views.py
@@ -4,8 +4,5 @@ from store.models import Product
 
 
 def say_hello(request):
-    # Sorts products by unit_price in ASC order and returns first object
-    queryset = Product.objects.earliest('unit_price')
-    # Sorts products by unit_price in DESC order and returns first object
-    queryset = Product.objects.latest('unit_price')
+    queryset = Product.objects.all()[:5]
     return render(request, 'hello.html', {'name': 'Andrew', 'products': list(queryset)})
