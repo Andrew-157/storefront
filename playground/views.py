095ebdc499de1e1975a36e357e0fffc53a1faa0d
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-06 10:59:03 +0200

    started using admin app

--- playground/views.py
+++ playground/views.py
@@ -4,7 +4,4 @@ from store.models import Order, OrderItem, Product, Customer, Collection
 
 
 def say_hello(request):
-    with connection.cursor() as cursor:
-        cursor.execute('')
-        cursor.callproc()  # this executes stored procedures
-    return render(request, 'hello.html', {'name': 'Andrew', 'result': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Andrew'})
